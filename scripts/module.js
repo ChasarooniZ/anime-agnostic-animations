9b7d084605f2ba4168dd9a40b394efec309eb1bc
cadowtin <chasdowtin@gmail.com>
2026-04-30 02:25:24 -0500

    fx handler

--- scripts/module.js
+++ scripts/module.js
@@ -1,13 +1,17 @@
 import { setupAPI } from "./api.js";
 import { setupSocket } from "./lib/socket.js";
+import { setupTokenMagicFX } from "./magicFXHandler.js";
+import { setupSettings } from "./settings.js";
 
 Hooks.once("init", async function () { });
 
 Hooks.once("setup", function () {
+  setupSettings()
   if (!setupSocket())
     console.error("Error: Unable to set up socketlib for Genga");
 });
 
 Hooks.once("ready", async function () {
+  setupTokenMagicFX()
   setupAPI();
 });
